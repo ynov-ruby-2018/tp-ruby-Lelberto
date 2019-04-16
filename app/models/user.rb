db7e1b3bc3908cc4d0c36e1ef10ce5b78e725cf7
Lelberto <jsurieux@gmail.com>
2019-04-16 10:17:36 +0200

    Added roles

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  # Rôle de l'utilisateur
+  enum role: [:user, :admin]
+
+  # Définit le rôle par défaut si l'utilisateur vient d'être créé
+  after_initialize :set_default_role, :if => :new_record?
+
+  # Définit le rôle par défaut
+  def set_default_role
+    self.role ||= :user
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_15_144921) do
+ActiveRecord::Schema.define(version: 2019_04_16_081247) do
 
   create_table "users", force: :cascade do |t|
     t.string "first_name"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2019_04_15_144921) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "role"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
