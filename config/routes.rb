318e64972e3ca83fbc6ac52e44e9dc96837df1a0
Rocknologie <theo.schmidt@outlook.fr>
2019-04-18 19:28:31 +0200

    Homepage/front

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  resources :listings
-  resources :categories
   root to: redirect("/#{I18n.default_locale}")
 
   scope "/:locale" do
     root 'homepage#index'
     devise_for :users
+    resources :listings
+    resources :categories
   end
 end
